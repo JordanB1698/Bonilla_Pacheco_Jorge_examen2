63f453f6b1a15187c9fe858457b8bac8e426c779
JordanB1698 <jbonillapacheco@gmail.com>
2022-02-09 19:17:52 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Bonilla_Pacheco_Jorge',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
