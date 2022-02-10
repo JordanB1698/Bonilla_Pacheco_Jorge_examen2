18ae8bd29e581e2b5916a7f238789279cbfdd151
JordanB1698 <jbonillapacheco@gmail.com>
2022-02-09 20:32:56 -0500

    Guardado 08:30

---
+++ src/app/Clases/clases.ts
@@ -0,0 +1,12 @@
+export class Cliente {
+    clienteId: number;
+    cedRucCliente: string;
+    nombreCliente: string;
+    apellidoCliente: string;
+    telefonoCliente: string;
+    direccionCliente: string;
+    email: string;
+    password: string;
+    estado: number;
+    facturas: any[];
+}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular/providers/toast-controller';
+import {  Cliente } from '../Clases/clases';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +10,49 @@ import { RouterLink } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: RouterLink) { }
+  constructor(private router: Router,
+    private toastCtrl:ToastController){}
+    //private clienteService: ClienteService) { }
+
+  email:string="";
+  pass:string="";
 
   ngOnInit() {
+  }
+
+  async presentToast(mesagge:string) {
+    const toast = await this.toastCtrl.create({
+      message: mesagge,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async ingresar(){
+
+
+
+    if(this.email.length < 1 || this.pass.length < 1){
+      this.presentToast("Ingresar el Usuario y Contraseña");
+      return;
+    }
+
+    
+    let cliente:Cliente = new Cliente();
+    cliente.email = this.email;
+    cliente.password = this.pass;
+
+    //const valid = await this.c.Login(conductor);
+    if(true){
+      this.router.navigate(['/tab1']);
+    }
+
+
   }
  
   registrarse(){
 
+    this.router.navigate(['/registrar-usuario']);
   }
 
 
